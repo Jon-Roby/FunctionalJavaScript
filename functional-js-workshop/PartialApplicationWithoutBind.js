2a2dcaabf60e8c92b5616989020befad3ad4c502
Jon Roby <jonathan.roby1@gmail.com>
2016-04-26 21:28:57 -0700

    Edit PartialApplicationWithoutBind.js

--- functional-js-workshop/PartialApplicationWithoutBind.js
+++ functional-js-workshop/PartialApplicationWithoutBind.js
@@ -3,6 +3,7 @@
 // Partial Application without Bind
 // Exercise 9 of 18
 //
+//
 // Partial application allows you to create new functions from existing functions, while fixing some number of arguments. After setting the arguments to be partially applied, you get a new function ready to take the rest of the arguments and perhaps execute the original function.
 //
 // More formally: Partial application refers to the process of fixing a number of arguments to a function, producing another function of smaller arity.
